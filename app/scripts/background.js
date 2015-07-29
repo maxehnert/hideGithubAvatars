961ede5dcdf8e98613b4c07c6b7e9f3de5bb720f
Max Ehnert <max.ehnert@gmail.com>
2015-07-29 19:55:21 -0400

    testing blocking img src url

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -7,3 +7,41 @@ chrome.runtime.onInstalled.addListener(function (details) {
 chrome.browserAction.setBadgeText({text: '\'Allo'});
 
 console.log('\'Allo \'Allo! Event Page for Browser Action');
+
+chrome.webRequest.onBeforeRequest.addListener(
+        function(details) {
+          return {cancel: details.url.indexOf("*://avatars1.githubusercontent.com/u/*/") != -1};
+        },
+        {urls: ["<all_urls>"]},
+        ["blocking"]);
+
+// 'use strict';
+//
+// chrome.webRequest.onBeforeRequest.addListener(
+//     function(details) {
+//         var currentTime = new Date();
+//         if(isOfficeTime(currentTime) && isWeekday(currentTime)){
+//             return {redirectUrl: chrome.extension.getURL('index.html')};
+//         }
+//         return details.url;
+//     },
+//     {
+//         urls: [
+//             "*://*.facebook.com/*",
+//             "*://*.twitter.com/*",
+//             "*://*.gmail.com/*",
+//         ],
+//         types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
+//     },
+//     ["blocking"]
+// );
+//
+// function isOfficeTime(currentTime){
+//     var hour = currentTime.getHours();
+//     return hour > 9 && hour < 18;
+// }
+//
+// function isWeekday(currentTime){
+//     var dayOfWeek = currentTime.getDay();
+//     return dayOfWeek >= 1 && dayOfWeek <= 5;
+// }
